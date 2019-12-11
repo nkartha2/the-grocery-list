13a8ef53ecfee6750977dc912ce837274d98818e
nkartha2 <nkartha2@gmail.com>
2019-12-11 12:25:55 -0500

    wip getting recipes to display

--- app/static/src/Recipes.tsx
+++ app/static/src/Recipes.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axiosClient from "./axiosClient";
 function Recipes(): JSX.Element {
+  const [recipeResults, setRecipes] = useState([]);
+
+  useEffect(() => {
+    try {
+      axiosClient({
+        method: "get",
+        url: "/api/v1/recipes"
+      }).then(
+        response => {
+          if (response.data && response.data.length > 0) {
+            setRecipes(response.data)
+          }
+        }
+      )
+    } catch(e) {
+      console.error(e);
+    }
+  })
+
   return (
     <div>
       <h2>Recipes</h2>
