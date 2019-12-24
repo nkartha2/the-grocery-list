9f7f6c76b1afb1d122daf4dbd077aaee5ff9de7d
nkartha2 <nkartha2@gmail.com>
2019-12-24 10:54:27 -0500

    wip button ui component

---
+++ app/static/src/ui_components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/_buttons.scss';
+
+type ButtonProps = {
+  color: string,
+  onClick: Function,
+  ctaString: string
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button
+      onClick={() => props.onClick()}
+    >
+      {props.ctaString}
+    </button>
+}
+
