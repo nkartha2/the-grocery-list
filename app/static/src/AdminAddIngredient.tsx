7b875e58c73d8b7703d0c9bad7ad451d5867c616
nkartha2 <nkartha2@gmail.com>
2020-01-04 16:41:36 -0500

    front end api admin add ing

--- app/static/src/AdminAddIngredient.tsx
+++ app/static/src/AdminAddIngredient.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import axiosClient from './axiosClient';
 
 function AdminAddIngredient(): JSX.Element {
+  async function addIng(ingName: string, ingType: string) {
+    try {
+      axiosClient({
+        method: "post",
+        url: "/api/v1/admin/ingredient",
+        params: {
+          "ingredient_name": ingName,
+          "ingredient_type": ingType
+        }
+      })
+    } catch(e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div className="form">
       <label>Ingredient Name</label>
