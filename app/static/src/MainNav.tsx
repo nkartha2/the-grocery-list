d5dc854d5527d661ede519b0102f7a6f888574cc
nkartha2 <nkartha2@gmail.com>
2020-02-11 16:11:57 -0500

    update main nav and recipe styling

--- app/static/src/MainNav.tsx
+++ app/static/src/MainNav.tsx
@@ -8,9 +8,12 @@ function MainNav() {
       <h1>Neel's Meals</h1>
       <FontAwesomeIcon style={{width: "100%", margin: "5px auto"}} icon={faCarrot} />
       <div className="menu">
-        <a>Recipes </a>
-        <a>Ingredients </a>
-        <a>Groceries </a>
+        <a href="/recipes">Recipes</a>
+        <h4>Admin</h4>
+        <a href="/admin/ingredient">Add Ingredient</a>
+        <a href="/admin/uom">Add Unit of Measure</a>
+        <a href="/admin/recipe">Add Recipe</a>
+        {/* <a href="">Groceries </a> */}
       </div>
     </div>
   )
