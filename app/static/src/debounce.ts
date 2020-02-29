d8119dcda4d34eb6e725b1696cd77ef0ac09964d
nkartha2 <nkartha2@gmail.com>
2020-02-29 17:54:00 -0500

    wip debounce

---
+++ app/static/src/debounce.ts
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function debounce(func: Function, wait: number) {
+  // should wait according to wait param
+  // to call the function
+  // setTimeout(func(), wait);
+
+  // clearTimeout to prevent the setTimeout to execute
+  // clearTimeout()
+}
