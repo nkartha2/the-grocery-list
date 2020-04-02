d61c80bc09b03a3855ec4b99014b2532f3da3980
Neely Kartha <nkartha2@gmail.com>
2020-04-02 13:12:48 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname("the_grocery_list"))
 
 DEBUG = True
-DATABASE_URI = 'postgres+psycopg2://postgres:Peanut94!!@127.0.0.1:5342/groceries'
+DATABASE_URI = 'postgres+psycopg2://postgres:@127.0.0.1:5342/groceries'
 CSRF_ENABLED = True
